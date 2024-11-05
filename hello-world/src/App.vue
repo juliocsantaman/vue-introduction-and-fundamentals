f79f1ac2ce1bd77fa0f34dd3d442ee20b2cf7af9
Julio Santaman <53060728+juliocsantaman@users.noreply.github.com>
2024-11-04 23:07:56 -0500

    Creating an app with create app name-app and creating a component in the project.

---
+++ hello-world/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <UserInfo />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import UserInfo from './components/UserInfo.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    UserInfo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ hello-world/src/components/UserInfo.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>User Info</h1>
+  <p>
+    Name: {{ name }}
+  </p>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: 'Julio Cesar'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
